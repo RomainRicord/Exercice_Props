6dbe1f84a66413b4f87083f541d8c66946aa5580
Romain Ricord <romain.ricord.pro@gmail.com>
2022-05-25 16:03:43 +0200

    FlatList !!!

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import {useState,useEffect} from 'react'
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,FlatList } from 'react-native';
 import Card from './src/component/Card';
 import data from './assets/data'
 import {SearchBar} from '@rneui/base'
@@ -46,38 +46,21 @@ export default function App() {
 
   return (
     <ScrollView>
-      <SearchBar  placeholder="ex. John DOE"
+      <SearchBar  placeholder="Recherche"
       onChangeText={(text) => searchFilterFunction(text)}
       value={search}
       inputContainerStyle={{
         flexDirection:'row-reverse'
       }}
       />
-   <View style={{display:'flex',flexDirection:'row',justifyContent:'center'}}>
-      <View style={{display:'flex',flexDirection:'column'}}>
-      {filteredDataSource.map((e,k) => {
-
-        if ((k+1)%2==1) {
-
-          return(
-            <Card key={k} kl={k} e={e} />
-          )
-        }
-
-      })}
-      </View>
-      <View style={{display:'flex',flexDirection:'column'}}>
-      {filteredDataSource.map((e,k) => {
-
-        if ((k+1)%2==0) {
-
-          return(
-            <Card key={k} kl={k} e={e} />
-          )
-        }
-
-      })}
-      </View>
+   <View style={{display:'flex',flexDirection:'column',justifyContent:'center',alignItems:'center'}}>
+   <FlatList
+  numColumns={2}
+  data={filteredDataSource}
+  renderItem={({ item, index, separators }) => (
+    <Card key={index} kl={index} e={item} />
+  )}
+  />
    </View> 
    </ScrollView>
   )
