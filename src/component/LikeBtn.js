d95fa216c4e432aef9a763b8b8ed1577c7a33bc4
Romain Ricord <romain.ricord.pro@gmail.com>
2022-05-30 10:11:50 +0200

    Ajout d'un petit peu de docs

--- src/component/LikeBtn.js
+++ src/component/LikeBtn.js
@@ -4,9 +4,11 @@ import { useEffect, useState } from 'react'
 
 const LikeBtn = ({like_}) => {
     
+    /* Checking if the like is undefined, if it is, it sets the like to 0. */
     const [like,setLike] = useState(typeof(like_) == "undefined" ? 0 : like_)
     const [favorite,setFavorite] = useState(false)
 
+    /* Checking if the like is less than or equal to 0, if it is, it sets the like to the like. */
     useEffect(() => {
 
         if (like <= 0){
@@ -19,6 +21,8 @@ const LikeBtn = ({like_}) => {
         <View style={{display:'flex',flexDirection:'row',justifyContent:'center',alignItems:'center'}}>
             <Pressable onPress={() => {
 
+                /* Checking if the favorite is true, if it is, it will subtract 1 from the like, if it
+                is not, it will add 1 to the like. */
                 setLike(Number(like)+(favorite ? -1 : 1))
                 setFavorite(!favorite)
 
